5e2b10cf52f4a7627861731309171d26a6b4ee87
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-09-05 13:28:08 +0900

    draw new chats on the client side

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,12 +9,38 @@ const formElement = getElementById('chat_form');
 
 // * global socket handler 
 socket.on('user_connected', (username) => {
-  console.log(`${username} connected!`)
+  drawNewChat(`${username} connected!`)
 })
+
+socket.on("new_chat", (data) => {
+  const {
+    chat,
+    username
+  } = data;
+  drawNewChat(`${username} : ${chat}`)
+})
+
+// * event callback functions 
+const handleSubmit = (event) => {
+  event.preventDefault(); // 기본적으로 form에서 submit을 할때 이벤트 버블이 발생한다.(새로고침이 됨) 이것을 방지하기 위해서 preventDefault로 이벤트의 기본값을 막는다. 
+  const inputValue = event.target.elements[0].value;
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    // 화면에 그리기 
+    drawNewChat(`me : ${inputValue}`)
+    event.target.elements[0].value = ''
+  }
+}
 
 // * draw functions 
 const drawHelloStranger = (username) => {
   (helloStrangerElement.innerHTML = `Hello ${username} Stranger :)`)
+}
+const drawNewChat = (message) => {
+  const wrapperChatBox = document.createElement('div')
+  const chatBox = `<div>${message}</div>`;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox)
 }
 
 function helloUser() {
@@ -27,6 +53,8 @@ function helloUser() {
 
 function init() {
   helloUser();
+  //이벤트 연결 
+  formElement.addEventListener('submit', handleSubmit)
 }
 
 init();
